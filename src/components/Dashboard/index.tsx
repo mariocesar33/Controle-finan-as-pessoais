56c7a59d7fadb21821932139b89a570a5b91aa81
Mário César Silva <mcgato33@hotmail.com>
2021-03-16 14:42:53 -0100

    criando componente: Summary

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,11 @@
+import { Summary } from '../Summary/index';
+
 import { Container } from './styles';
 
 export function Dashboard () {
   return (
     <Container>
-
+      <Summary />
     </Container>
   );
 }
