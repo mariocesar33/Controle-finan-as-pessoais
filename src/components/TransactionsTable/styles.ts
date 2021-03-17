63e63df0cee2f7a25bb4bcf130536c0593b6f22f
Mário César Silva <mcgato33@hotmail.com>
2021-03-17 16:22:32 -0100

    criando componente: TransactionsTable

---
+++ src/components/TransactionsTable/styles.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  margin-top: 4rem;
+
+  table {
+    width: 100%;
+    border-spacing: 0 0.5rem;
+
+    thead th {
+      color: var(--text-body);
+      font-weight: 400;
+      padding: 1rem 2rem;
+      text-align: left;
+      line-height: 1.5rem;
+    }
+
+    tbody td {
+      padding: 1rem 2rem;
+      border: 0;
+      background: var(--shape);
+      color: var(--text-body);
+      border-radius: 0.25rem;
+
+      &:first-child { //pega todos os primeiros <td> dentro de cada <tr>
+        color: var(--text-title);
+      }
+
+      &.deposit {
+        color: var(--green);
+      }
+
+      &.withddram {
+        color: var(--red);
+      }
+    }
+  }
+`;
