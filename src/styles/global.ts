35f13f6c0e848925a33ca57ddb63a55a1c9c6a0b
Mário César Silva <mcgato33@hotmail.com>
2021-03-22 03:48:34 -0100

    Update global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -56,8 +56,24 @@ export const GlobalStyle = createGlobalStyle`
 
   .react-modal-overlay {
     background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
   .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    padding: 3rem;
 
+    background: var(--background);
+    border-radius: 0.24rem;
+    position: relative;
   }
 `;
